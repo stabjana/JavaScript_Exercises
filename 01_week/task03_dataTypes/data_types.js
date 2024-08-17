08a8bb7b8b2501a05ce92633b9c76ecb83cc384b
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-08-17 20:21:25 +0300

    added task03 - completed

--- 01_week/task03_dataTypes/data_types.js
+++ 01_week/task03_dataTypes/data_types.js
@@ -1,12 +1,33 @@
 let age = 30;
-let name = 'Basilisk';
+let Name = 'Basilisk';
 let isStudent = false;
 let hobbies = ['Tennis', 'Trampoline', 'future guessing']; //Declaration and Assignment of the variables
+              // string array is seperated by commas
 
 let info = {
-    nameOfSchool: 'Hogwarts'    //Creating an Object that is changeable because you might want to add another info or you want to change the grade. 
-    grade = 5
+    nameOfSchool: 'Hogwarts',    //Creating an Object that is changeable because you might want to add another info or you want to change the grade. 
+    grade: 5
 }
-// I used https://www.geeksforgeeks.org/creating-objects-in-javascript-4-different-ways/#using-object-literals to help me out with that.  NOCHMAL NACHLESEN!!!
+// I used https://www.geeksforgeeks.org/creating-objects-in-javascript-4-different-ways/#using-object-literals to help me out with that.
+/* info object is declared like a variable, because it contains variables. Curly brackets are showing where the dictionary starts and ends, so it will be read and the variables are initiated */
 
-// Dann den Rest noch machen.
+console.log(age);
+console.log(Name);
+console.log(isStudent);
+console.log(hobbies);
+console.log(info);
+
+
+
+/*
+- Create a variable named age and assign it an integer value.
+- Create a variable named name and assign it a string value.
+- Create a variable named isStudent and assign it a boolean value.
+
+- Create a variable named hobbies and assign it an array containing a few string values (e.g., hobbies).
+
+- Create an info object with two properties: schoolName (a string) and grade (an integer).
+
+- Use console.log() to print each variable to the console.
+
+- Use the typeof operator to check the data type of each variable and print the result using console.log(). */
