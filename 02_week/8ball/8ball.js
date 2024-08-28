a97ce04f9643bf623cd2368466fe1841ddb6250c
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-08-28 10:09:58 +0300

    added new folder 03_week

--- 02_week/8ball/8ball.js
+++ 02_week/8ball/8ball.js
@@ -1,10 +1,36 @@
 // Create a Magic 8-Ball program that emulates the classic fortune-telling toy. The user will input a yes-or-no question, and the program will output a random response from the 8-Ball. Implement the program using conditional statements to select and display one of several predefined responses.
 
 
+const answer = ["Make it so.", "You're in love with a computer subroutine.", " The only thing to do with good advice is to pass it on. It is never of any use to oneself.", "TA good scare is worth more to a man than good advice.", "When you encounter seemingly good advice that contradicts other seemingly good advice, ignore them both.", "The Golden Rule will always be good advice!"]
+// array with possible answers
 
-
-
-
-function convertStringToNumber(string) {
-    return parseInt(string)
+function createMessage() {
+    return answer[Math.floor(Math.random() * 4)];
 }
+// Funktion gibt die random Antwort aus
+
+function answer(answer) {
+    alert(answer);
+}
+
+document.querySelector(`#submit_button`).onclick = function () {
+    window.antwort.text = 'hallo';
+}
+/* ERROR: 8ball.js:13 Uncaught TypeError: Cannot set properties of null (setting 'onclick')
+at 8ball.js:13:49 */
+
+// Funktion für den klick des buttons
+
+
+
+// Funktion für display der Antwort
+
+/* console.log In das Antwort-Feld rein 
+und das Eingabefeld muss die Frage aufnemhen 
+
+  <button class="submit_answer_button" id="submit_button" style="display:none;">Eingabe</button>
+
+
+
+*/
+
---
+++ 03_week/README.md
@@ -0,0 +1,22 @@
+2024_08_28
+JavaScript
+
+Loops
+
+Loops are used to repeat a block of code multiple times
+Useful for iterating over collections, repeating tasks, and automating repetitive processes
+JavaScript provides several types of loops to handle different situations
+
+We have multiple diff loops, main idea is the same
+
+for loops are really good at first, its old but does the task
+
+for loop
+for (initilization; condition; increment) {
+}
+
+for (let i = 0; i < 5; i++) {
+console.log(i);
+}
+
+first parameter is a variable that is counting
