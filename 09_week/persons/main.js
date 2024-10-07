7b0d089793ac6b56763d4699085d2d39aa8ce0d8
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-10-07 20:30:39 +0300

    add cats and add persons task

--- 09_week/persons/main.js
+++ 09_week/persons/main.js
@@ -1,3 +1,34 @@
-fetch('https://jsonplaceholder.typicode.com/todos/2')
+/*
+fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then((json) => {
+        json.forEach((aName) => console.log(aName.name));
+    });
+
+
+or getting other values from the array and the objects inside
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((json) => {
+        json.forEach((aName) => console.log(aName.name.city));
+    }); */
+
+
+catFrame = document.querySelector('#catFrame')
+
+async function fetchCats() {
+    try {
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search`)
+        const data = await response.json()
+        console.log(data)
+
+        catFrame.style.width = `${data[0].width}px`;
+        catFrame.style.height = `${data[0].height}px`;
+        catFrame.src = `${data[0].url}`;
+
+    } catch (error) {
+        console.error('Error', error);
+    }
+}
+fetchCats();
