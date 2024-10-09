a3de8cd5d358b2e7351f6d1c18c7bc0fb3ca5d82
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2024-10-09 22:26:25 +0300

    add persons JS code finished

--- 09_week/persons/main.js
+++ 09_week/persons/main.js
@@ -1,34 +1,49 @@
-/*
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then((json) => {
-        json.forEach((aName) => console.log(aName.name));
-    });
+let persons = [];
 
+class User {
+    constructor(id, name, username, email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+}
 
-or getting other values from the array and the objects inside
-
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then((json) => {
-        json.forEach((aName) => console.log(aName.name.city));
-    }); */
-
-
-catFrame = document.querySelector('#catFrame')
-
-async function fetchCats() {
+async function getUserInfo() {
     try {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search`)
-        const data = await response.json()
-        console.log(data)
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json(); // why again await?
+        /* console.log(data); */
 
-        catFrame.style.width = `${data[0].width}px`;
-        catFrame.style.height = `${data[0].height}px`;
-        catFrame.src = `${data[0].url}`;
-
+        data.forEach((person) => { // so nenn ich das array element was grade bearbeitet wird
+            persons.push(new User(person.id, person.name, person.username, person.email))
+        });
     } catch (error) {
         console.error('Error', error);
     }
+    /*  console.log(persons); */
+
+    crateCards();
 }
-fetchCats();
+getUserInfo();
+
+
+function crateCards() {
+    const container = document.getElementById('card-container');
+    console.log(persons);
+
+    persons.forEach(person => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+         <img src="https://robohash.org/${person.id}.png?set=set4">
+        <h3>${person.username}</h3>
+        <p>${person.name}</p>
+        <p>${person.email}</p>
+        `
+        /* console.log(card); */
+        container.appendChild(card);
+        /* Erzeug connection zu div, erzeug class für div, änder den Inhalt
+        IMG: Anleitung in der Website, URL + die person id + das Katzenset */
+    })
+}
